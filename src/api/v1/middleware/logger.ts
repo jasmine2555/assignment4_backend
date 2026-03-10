84a52a31305f77f02533c57dab46776652bcc2a1
Jasmine <jasminekaur9@academic.rrc.ca>
2026-03-09 20:54:48 -0500

    Add request logging middleware

---
+++ src/api/v1/middleware/logger.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+
+export const logger = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+};
